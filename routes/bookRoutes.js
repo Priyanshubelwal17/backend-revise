b9db4d1467d23bbb4d64dab67be17c27c13b3096
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-09-11 22:42:44 +0530

    make Book route file for bookfolder

---
+++ routes/bookRoutes.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const Book = require("../models/Book");
+const router = express.Router();
+
+
+
+
+
+router.post("/", async (req, res) => {
+    try {
+        const newBook = new Book(req.body);
+        await newBook.save()
+        res.status(201).json(newBook)
+    } catch {
+        res.status(500).json({ error: err.message })
+    }
+})
+
+router.get("/", async (req, res) => {
+    const books = await Book.find();
+    res.json(books)
+})
+
+router.get("/:id", async (req, res) => {
+    const book = await Book.findById(req.params.id);
+    if (!book) res.status(404).json({ error: "Book not found" })
+    res.json(book)
+})
+
+router.put("/:id", async (req, res) => {
+    const updatedBook = await Book.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!updatedBook) res.status(404).json({ error: "Book not found" })
+    res.json(updatedBook)
+})
+router.delete("/:id", async (req, res) => {
+    const deletedBook = await Book.findByIdAndDelete(req.params.id);
+    if (!deletedBook) res.status(404).json({ error: "Book not found" })
+    res.json({ message: "Book deleted", deleted: deletedBook })
+})
+
+module.exports = router
